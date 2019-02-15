e4b7d14d1d01f8f01244e7ff7aea68adfebe9518
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-02-14 16:50:19 -0800

    Add pod_init for sdsc-merra2

---
+++ k8s/sdsc-merra2/pod_init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Point reverse-DNS to the service DNS name, to make xrootd happy
+cp /etc/hosts /etc/hosts.1 && (cat /etc/hosts.1 | sed 's/stashcache-origin-merra2.*/stashcache-origin-merra2.nautilus.optiputer.net/g' >/etc/hosts)
+
