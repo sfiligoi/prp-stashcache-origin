d59e2c043d9bd195d60398d1d0c81f6ad4252b7f
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-02-14 16:51:48 -0800

    Change also HOSTNAME

--- k8s/sdsc-merra2/pod_init.sh
+++ k8s/sdsc-merra2/pod_init.sh
@@ -3,3 +3,4 @@
 # Point reverse-DNS to the service DNS name, to make xrootd happy
 cp /etc/hosts /etc/hosts.1 && (cat /etc/hosts.1 | sed 's/stashcache-origin-merra2.*/stashcache-origin-merra2.nautilus.optiputer.net/g' >/etc/hosts)
 
+export HOSTNAME=stashcache-origin-merra2.nautilus.optiputer.net
